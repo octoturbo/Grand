1785cf72515d6e71d273430fc5f77dbc8a9f50d7
octoturbo <6717325+octoturbo@users.noreply.github.com>
2022-12-21 16:34:29 -0600

    somemore codes

--- js/site.js
+++ js/site.js
@@ -1,4 +1,45 @@
-//Call Hello World
-function HelloWorld(){
-    alert("hellow World");
+//get the values from the Page
+//starts or controller function
+function getValues(){
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //parse into Integers
+    startValue = parseInt(startValue); 
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)){
+        //we call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+    } else {
+        alert("You must enter integers");
+
+    }
+
+    
+ 
+    //we call displayNumbers
+}
+
+//generate numbers from startvalue to the endvalue
+//logic function(s)
+function generateNumbers(sValue, eValue){
+
+    let numbers = [];
+
+    //we want to get all numbers from start to end
+
+    for(let index = sValue; index <= eValue; index++){
+        //this will execute in a loop until index = eValue
+        numbers.push(index);
+    }
+    return numbers;
+
+}
+
+//display the numbers and mark even numbers bold
+//display or view functions
+function displayNumbers(){
+
 }
